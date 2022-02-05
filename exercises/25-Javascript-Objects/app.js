ea822d44033788b89cf6a22e5399de64dd970875
MaiCodes-exe <maise.b.costa@gmail.com>
2022-02-05 11:13:24 +0000

    last two

--- exercises/25-Javascript-Objects/app.js
+++ exercises/25-Javascript-Objects/app.js
@@ -3,10 +3,9 @@ var person = {
     lastname: "Doe",
     age: 35,                     //Number
     gender: "male",
-    lucky_numbers: [ 7, 11, 13, 17], //Array
+    lucky_numbers: [ 7, 11, 13, 17], //Array -> [7,11,13,33]
     significant_other: person2   //Object, yes the same variable/object defined after
 };
-
 var person2 = {
     name: "Jane",
     lastname: "Doe",
@@ -15,24 +14,31 @@ var person2 = {
     lucky_numbers: [ 2, 4, 6, 8],
     significant_other: person
 };
-
+var person3 ={
+    name: "Jimmy",
+    lastname: "Doe",
+    age: 13,
+    gender: "male",
+    lucky_numbers: [ 1, 2, 3, 4],
+    significant_other: null
+}
 var family = {
     lastname: "Doe",
-    members: [person, person2]   //Array of objects, don't forget to add Jimmy
+    members: [person, person2, person3]   //Array of objects, don't forget to add Jimmy
 };
-
-
 function addAllFamilyLuckyNumbers(anArray){
-  var sumOfAllLuckyNumbers = 0; //sumOfAllLuckyNumbers is a number, the sum of all lucky numbers.
-
-  //To-Do: loop and add; consider nested loops
-  //Hint: use the anArray variable to get all of the lucky numbers
-  
-  return sumOfAllLuckyNumbers;
+    var sumOfAllLuckyNumbers = 0; //sumOfAllLuckyNumbers is a number, the sum of all lucky numbers.
+    for (let i = 0; i < anArray.length; i++){
+        for (let a = 0; a < anArray[i].lucky_numbers.length; a++){
+            sumOfAllLuckyNumbers += anArray[i].lucky_numbers[a];
+            //console.log(sumOfAllLuckyNumbers);
+        }
+    }
+    //console.log(sumOfAllLuckyNumbers)
+    return sumOfAllLuckyNumbers;
 }
-
 //Enter all your code here:
-
-
+person.lucky_numbers[3] = 33;
 //Do not make changes below:
-console.log(addAllFamilyLuckyNumbers(family.members)); 
+console.log(addAllFamilyLuckyNumbers(family.members));
+// Output should be 94
